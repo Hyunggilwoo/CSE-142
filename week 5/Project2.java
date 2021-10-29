84ff277220f4b6c6f7031f1b6de8b8c4f4e1edfd
Hyunggil Woo <hyunggilswoo@gmail.com>
2021-10-28 21:55:45 -0700

    commented methods in Project2.

--- week 5/Project2.java
+++ week 5/Project2.java
@@ -42,69 +42,68 @@ public class Project2 {
     return EmployeeCnt;
   }
 
-  /**
-  * Continues to process the total pay for each employees
-  * until all employees are processed.
-  * 
-  * @param theConsole to prompt for inputs to the questions
-  * @param theEmployeeCnt the number of times to process the pay
-  */
-  public static void processEmployeePay(Scanner theConsole, int theEmployeeCnt) {
-    for (int employee = 1; employee <= theEmployeeCnt; employee++) {
-      System.out.println("Employee Name: ");
-      String name = theConsole.next();
-      System.out.println("Hours Worked: ");
-      double hours = theConsole.nextInt();
-      System.out.println("Pay Rate: ");
-      double payRate = theConsole.nextInt();
-      System.out.println("Gross Pay: ");
-      double grossPay = getGrossPay(hours, payRate);
-    }
-  }
+//   /**
+//   * Continues to process the total pay for each employees
+//   * until all employees are processed.
+//   * 
+//   * @param theConsole to prompt for inputs to the questions
+//   * @param theEmployeeCnt the number of times to process the pay
+//   */
+//   public static void processEmployeePay(Scanner theConsole, int theEmployeeCnt) {
+//     for (int employee = 1; employee <= theEmployeeCnt; employee++) {
+//       System.out.println("Employee Name: ");
+//       String name = theConsole.next();
+//       System.out.println("Hours Worked: ");
+//       double hours = theConsole.nextInt();
+//       System.out.println("Pay Rate: ");
+//       double payRate = theConsole.nextInt();
+//       System.out.println("Gross Pay: ");
+//       double grossPay = getGrossPay(hours, payRate);
+//     }
+//   }
   
-  /**
-  * Calculates the employee's pay that also accounts 
-  * for the overtime pay.
-  * 
-  * @param theHours The number of hours the employee worked
-  * @param thePayRate The employee's pay rate
-  * @return The weekly gross pay
-  */
-  public static double getGrossPay(double theHours, double thePayRate) {
-    double result = 0.00;
-    if ( theHours <= 40.00) {
-//       result = theHours * thePayRate;
-      result = under40(theHours, thePayRate);
-    }
-    else if (theHours > 40.00 && theHours <= 48.00) {
-      result = (theHours - 40.00) * 1.5 * thePayRate + 40 * thePayRate;
-    } else {
-      result = (theHours - 48) * 2 * thePayRate + (theHours - 40.00) * 1.5 * thePayRate + 40 * thePayRate;
-    }
-    return result;
-  }
+//   /**
+//   * Calculates the employee's pay that also accounts 
+//   * for the overtime pay.
+//   * 
+//   * @param theHours The number of hours the employee worked
+//   * @param thePayRate The employee's pay rate
+//   * @return The weekly gross pay
+//   */
+//   public static double getGrossPay(double theHours, double thePayRate) {
+//     double result = 0.00;
+//     if (theHours <= 40.00) {
+//       result = under40(theHours, thePayRate);
+//     }
+//     else if (theHours > 40.00 && theHours <= 48.00) {
+//       result = (theHours - 40.00) * 1.5 * thePayRate + 40 * thePayRate;
+//     } else {
+//       result = (theHours - 48) * 2 * thePayRate + (theHours - 40.00) * 1.5 * thePayRate + 40 * thePayRate;
+//     }
+//     return result;
+//   }
   
-  /**
-  * Calculates the pay that is worked less than 40 hours.
-  *
-  * @param theHours The number of hours the employee worked
-  * @param thePayRate The employee's pay rate
-  * @return The weekly gross pay
-  */
-  public static double under40 (double theHours, double thePayRate) {
-    return theHours * thePayRate;
-  }
+//   /**
+//   * Calculates the pay that is worked less than 40 hours.
+//   *
+//   * @param theHours The number of hours the employee worked
+//   * @param thePayRate The employee's pay rate
+//   * @return The weekly gross pay
+//   */
+//   public static double under40 (double theHours, double thePayRate) {
+//     return theHours * thePayRate;
+//   }
 
-  /**
-  * Calculates the pay that is worked less than 48 hours.
-  *
-  * @param theHours The number of hours the employee worked
-  * @param thePayRate The employee's pay rate.
-  * @return The weekly gross pay.
-  */
-  public static double under48 (double theHours, double thePayRate) {
-    double result = under40(theHours, thePayRate);
-    result += (theHours - 40.00) * 1.5 * thePayRate;
-    return result;
-  }
+//   /**
+//   * Calculates the pay that is worked less than 48 hours.
+//   *
+//   * @param theHours The number of hours the employee worked
+//   * @param thePayRate The employee's pay rate.
+//   * @return The weekly gross pay.
+//   */
+//   public static double under48 (double theHours, double thePayRate) {
+//     double result = under40(theHours, thePayRate);
+//     result += (theHours - 40.00) * 1.5 * thePayRate;
+//     return result;
+//   }
 }
