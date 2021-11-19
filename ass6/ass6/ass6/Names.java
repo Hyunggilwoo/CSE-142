930eb9d283d95a9c62ace0a27ed3e23308a02def
Hyunggil Woo <hyunggilswoo@gmail.com>
2021-11-18 23:11:41 -0800

    drawGraph() correctly prints graph.

--- ass6/ass6/ass6/Names.java
+++ ass6/ass6/ass6/Names.java
@@ -4,14 +4,14 @@ import java.awt.*;
 
 public class Names {
 
-    public static final int NUMBER_OF_DECADES = 14;
+    public static final int NUMBER_OF_DECADES = 10; //14 
     
-    public static final int FIRST_YEAR = 1880;
+    public static final int FIRST_YEAR = 1920; //1880
     
     public static final int HORIZONTAL_WIDTH = 70;
     
     public static void main(String[] theArgs) throws FileNotFoundException {
-        Scanner input = new Scanner(new File("names.txt")); //"names2.txt"
+        Scanner input = new Scanner(new File("names2.txt")); //"names2.txt"
         Scanner console = new Scanner(System.in);
         intro();
         System.out.print("name? ");
@@ -40,18 +40,11 @@ public class Names {
     * @return the line (if there is one)
     */
     public static String find(Scanner input, String theName, String theSex) {
-        while (!input.equals("")) { // input.hasNext()
-            String line = "";
-            String name = input.next();
-            String sex = input.next();
-//             String ranks = input.nextLine();
-            if (name.toLowerCase().equals(theName.toLowerCase())
-                && sex.toLowerCase().equals(theSex.toLowerCase())) {
-                while (!input.equals("")) {
-                    String ranks = input.next();
-                    line = ranks + " ";
-                }
-                return name + " " + sex + " " + line;
+        while (input.hasNextLine()) { //!line.equals("")
+            String line = input.nextLine();
+            if (line.toLowerCase().contains(theName.toLowerCase())
+                && line.toLowerCase().contains(theSex.toLowerCase())) {
+                return line;
             }  
         }
         return "";
@@ -71,20 +64,21 @@ public class Names {
             System.out.print( "(" + name + " " + sex + " " + year + ", " + rank + ") ");
         }
     }
-    // The horizontal line at the top is 25 pixels below the top;
-    // the horizontal line at the bottom is 25 pixels above the bottom;
-    // The panel is divided into the NUMBER_OF_DECADES;
-    // These sections have horizontal width of 70 pixels each
-    // Vertical line to the left of each of the 14 sections is drawn
-    // including x coordinate of 0;
-    // Decades are labeled at the bottom panel using drawString method;
-    // The int value of the year is turned into a String by
-    // concatenating empty String as "" + 1880;
-    // There are 500 pixels to plot the values; The rank range from
-    // 1 to 1000 (i.e. rank of 1 or 2 should be drawn at y - coordiante
-    // of 25, rank of 3 or 4 should be drawn at y - coordinate of 26...)
-    // up to ranks of 999 to 1000 should be drawn at y-coordinate of 524;
-    // A rank of 0 should be printed at the very bottom (525);
+    
+    /*
+    *  Pre: The first token is a string of a name, the second token is either
+    *  M or F, then the rest of the tokens in the line should be integers 
+    *  indicating the ranks. The rank must be between 1 and 1000.
+    *  (If rank is greater than 1000 or a negative number, incorrect output
+    *  is given.)
+    *
+    *  Post: Uses the DrawingPanel and accepts a scanner of a line of text.
+    *  Prints a name on each year depending on their ranking. Graph is 
+    *  printed with a line connecting the ranks of the name to its
+    *  adjacent names on the graph. The lower number in ranking is printed 
+    *  higher on on y-axis. If the name was not ranked, it will be given 
+    *  a rank of 0.
+    */
     public static void drawGraph(Graphics g, String theLine) {
         Scanner data = new Scanner(theLine);
         int year = FIRST_YEAR;
@@ -94,19 +88,24 @@ public class Names {
         g.drawLine(0, 525, NUMBER_OF_DECADES * HORIZONTAL_WIDTH, 525);
         g.drawLine(0, 0, 0, 550);
         String totalRank = "";
-//         int rank = data.nextInt();
-        // {# of token > 0}
         while (data.hasNext()) {
-            for (int num = 0; num < NUMBER_OF_DECADES; num++) {
-                int rank = data.nextInt();
+            // fencepost: 
+            int rank = data.nextInt();
+            g.drawLine(0, 0, 0, 550);
+            g.drawString(String.valueOf(year), 0, 550); 
+            g.drawString(name + " " + sex + " " + rank, 0, findY(rank));
+            int temp = findY(rank); // value used to connect values on a line      
+            for (int num = 1; num < NUMBER_OF_DECADES; num++) {
+                rank = data.nextInt();
+                year+=10; 
                 g.drawLine(70 * num, 0, 70 * num, 550);
-                g.drawString(String.valueOf(year), 70 * num, 550);
-                year+=10;
+                g.drawLine(70 * (num - 1), temp, 70 * num, findY(rank)); 
+                temp = findY(rank); // reassigning temp to the rank in a previous line
+                g.drawString(String.valueOf(year), 70 * num, 550); 
                 g.drawString(name + " " + sex + " " + rank, 70 * num, findY(rank));    
-            }
+            } 
 
-        }
-        // {# of token = 0}
+        } 
         
     }
     
@@ -125,22 +124,7 @@ public class Names {
         }
         return result; 
     }
-        /** {rank > 0, rank = {2, 4, ... 999} result = {25, 26, ... 524} 
-                        1                       25                          (?? * 1) = 1
-                        2                       25                      (int) 24 + (?? * rank) = 25
-                                                                            (?? * 2) = 1
-                                                                            ?? = 1/2
-                                                                            
 
-                        4                       26
-                                                
-                        1000                    524                     (int) 24 + (?? * 1000) = 524
-                                                                            (?? * 1000) = 500
-                                                                            ?? = 500/1000
-                                                                            ?? = 0.5
-                                                                        
-        *    
-        **/ 
     
     /**
     * Introduces the game to the user.
