49ff093496486689f7a273e369d2647254759b63
Hyunggil Woo <hyunggilswoo@gmail.com>
2021-11-19 23:50:15 -0800

    Refactored drawGraph() method into following methods: drawBorders() and nameLabel().

--- ass6/ass6/ass6/Names.java
+++ ass6/ass6/ass6/Names.java
@@ -25,6 +25,8 @@ public class Names {
     
     public static final int HORIZONTAL_WIDTH = 70;
     
+    public static final int HEIGHT = 550;
+    
     public static void main(String[] theArgs) throws FileNotFoundException {
         Scanner input = new Scanner(new File("namesWithS.txt")); //"names.txt"
         Scanner console = new Scanner(System.in);
@@ -35,41 +37,19 @@ public class Names {
         String sex = console.next();
         String line = find(input, name, sex);
         if (line.length() > 0) {
-            print(line);
+            // print(line);
             DrawingPanel panel = new DrawingPanel(NUMBER_OF_DECADES * 
-                                                HORIZONTAL_WIDTH, 550);
+                                                HORIZONTAL_WIDTH, HEIGHT);
             Graphics g = panel.getGraphics();
             drawGraph(g, line);
         } else {
             System.out.println("name/sex combination not found");
         }
     }
-    
-    /**
-    *  Pre: Expects the method to return the line that has the name and the sex.
-    *  The method is not parsing through the entire text files. 
-    * 
-    *  Post: Returns a line that contains the name and the sex. This method passes
-    *  the String to find method.
-    *
-    * @param input scans the entire input file
-    * @param the name given to examine
-    * @return the line (if there is one)
-    */
-    public static String findLine(Scanner input, String theName, 
-                                    String theSex) {
-        while (input.hasNextLine()) { //!line.equals("")
-            String line = input.nextLine();
-            if (line.toLowerCase().contains(theName.toLowerCase())
-                && line.toLowerCase().contains(theSex.toLowerCase())) {
-                return line;
-            }  
-        }
-        return "";
-    }
 
     /**
-    *  Pre: Accepts a String from a findLine().
+    *  Pre: Expects the method to return the line that has the name and the sex.
+    *  The method is not parsing through the entire text files.
     * 
     *  Post: Return the correct line of input
     *  containing the exact name and sex on the line.
@@ -78,36 +58,36 @@ public class Names {
     * @param the name given to examine
     * @return the line (if there is one)
     */    
-    public static String find(String line, String theName, String theSex) {
+    public static String find(Scanner input, String theName, String theSex) {
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
             Scanner console = new Scanner(line);
             while (console.hasNext()) {
                 String name = console.next();
-                System.out.println(name); //
                 String sex = console.next();
-
                 if (name.toLowerCase().equals(theName.toLowerCase())
                     && sex.toLowerCase().equals(theSex.toLowerCase())) {
                         return line;
-                } // {return line 
-            } //{""} 
-
+                } 
+            }  
+        }
         return "";
     }
     
-    // Prints the ranks of the popular name in each decade
-       
-    public static void print(String theLine) {
-        Scanner data = new Scanner(theLine);
-        int year = FIRST_YEAR - 10;
-        String name = data.next();
-        String sex = data.next();
-        String totalRank = "";
-        for (int num = 0; num < NUMBER_OF_DECADES; num++) {
-            int rank = data.nextInt();
-            year+=10;
-            System.out.print( "(" + name + " " + sex + " " + year + ", " + rank + ") ");
-        }
-    }
+//     // Prints the ranks of the popular name in each decade
+//        
+//     public static void print(String theLine) {
+//         Scanner data = new Scanner(theLine);
+//         int year = FIRST_YEAR - 10;
+//         String name = data.next();
+//         String sex = data.next();
+//         String totalRank = "";
+//         for (int num = 0; num < NUMBER_OF_DECADES; num++) {
+//             int rank = data.nextInt();
+//             year+=10;
+//             System.out.print( "(" + name + " " + sex + " " + year + ", " + rank + ") ");
+//         }
+//     }
     
     /*
     *  Pre: The first token is a string of a name, the second token is either
@@ -128,29 +108,54 @@ public class Names {
         int year = FIRST_YEAR;
         String name = data.next();
         String sex = data.next();
-        g.drawLine(0, 25, NUMBER_OF_DECADES * HORIZONTAL_WIDTH, 25);
-        g.drawLine(0, 525, NUMBER_OF_DECADES * HORIZONTAL_WIDTH, 525);
-        g.drawLine(0, 0, 0, 550);
+        drawBorders(g);        
         String totalRank = "";
         while (data.hasNext()) {
-            // fencepost: 
             int rank = data.nextInt();
-            g.drawLine(0, 0, 0, 550);
-            g.drawString(String.valueOf(year), 0, 550); 
-            g.drawString(name + " " + sex + " " + rank, 0, findY(rank));
+            // fencepost: 
+            g.drawLine(0, 0, 0, HEIGHT);
+            g.drawString(String.valueOf(year), 0, HEIGHT);
+            nameLabel(g, name, sex, rank, 0); // printing the first vertical line
+//             g.drawString(name + " " + sex + " " + rank, 0, findY(rank));
             int temp = findY(rank); // value used to connect values on a line      
             for (int num = 1; num < NUMBER_OF_DECADES; num++) {
                 rank = data.nextInt();
                 year+=10; 
-                g.drawLine(70 * num, 0, 70 * num, 550);
-                g.drawLine(70 * (num - 1), temp, 70 * num, findY(rank)); 
+                g.drawLine(HORIZONTAL_WIDTH * num, 0, HORIZONTAL_WIDTH * num, HEIGHT);
+                g.drawLine(HORIZONTAL_WIDTH * (num - 1), temp, HORIZONTAL_WIDTH * num, findY(rank)); 
                 temp = findY(rank); // reassigning temp to the rank in a previous line
-                g.drawString(String.valueOf(year), 70 * num, 550); 
-                g.drawString(name + " " + sex + " " + rank, 70 * num, findY(rank));    
+                g.drawString(String.valueOf(year), HORIZONTAL_WIDTH * num, HEIGHT);
+                nameLabel(g, name, sex, rank, HORIZONTAL_WIDTH * num);     
             } 
 
         } 
         
+    }
+    
+    /**
+    * Draws a bottom border and the top border on the graph.
+    *
+    * @param g graphics drawingPanel object
+    *
+    */
+    public static void drawBorders(Graphics g) {
+        g.drawLine(0, 25, NUMBER_OF_DECADES * HORIZONTAL_WIDTH, 25);
+        g.drawLine(0, HEIGHT - 25, NUMBER_OF_DECADES * HORIZONTAL_WIDTH, 
+                                                            HEIGHT - 25);    
+    }
+    
+    /**
+    * Prints the name, sex, rank of the name on a graph.
+    * @requires only num >= 0 
+    * @param g is a drawingPanel object
+    * @param num is the location of x axis on the graph
+    * @param name the name of the character
+    * @param rank is the number in rank
+    * @return name, sex, rank of the name.
+    */
+    public static void nameLabel(Graphics g, String name, String sex, 
+                                                    int rank, int num) {
+        g.drawString(name + " " + sex + " " + rank, num, findY(rank));
     }
     
     /**
